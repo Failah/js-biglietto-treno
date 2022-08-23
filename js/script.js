9d6899ecd884c56d38a3582ca45ebd4ebc8d78b0
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-23 15:05:13 +0200

    Create the programm

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-const km = parseInt(prompt("Quanti km vuoi percorrere?"));
-const age = parseInt(prompt("Quanti anni hai?"))
+const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+const age = parseInt(prompt("Quanti anni hai?"));
+
+const ticketPrice = km * 0.21;
 
 console.log("km", km);
 console.log("age", age);
 
+if (age < 18) {
+    document.getElementById("price").innerHTML = ticketPrice * 4 / 5;
+} else if (age > 64) {
+    document.getElementById("price").innerHTML = ticketPrice * 3 / 5;
+} else {
+    document.getElementById("price").innerHTML = ticketPrice;
+}
