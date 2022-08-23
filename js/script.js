9302492b8b79e2b600d73061d8635e1462484da9
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-23 16:19:17 +0200

    Added a better solution in JS

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
+// SVOLGIMENTO 1
+
+// const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
+// const age = parseInt(prompt("Quanti anni hai?"));
+
+// const ticketPrice = (km * 0.21).toFixed(1);
+
+// console.log("km", km);
+// console.log("age", age);
+
+// if (!isNaN(km) && !isNaN(age)) {
+//     if (age < 18) {
+//         document.getElementById("price").innerHTML = ticketPrice * 4 / 5 + " Euro";
+//     } else if (age > 64) {
+//         document.getElementById("price").innerHTML = ticketPrice * 3 / 5 + " Euro";
+//     } else {
+//         document.getElementById("price").innerHTML = ticketPrice + " Euro";
+//     }
+// }
+// else {
+//     document.getElementById("price").innerHTML = "Non hai inserito un numero!";
+// }
+
+
+// SVOLGIMENTO 2 MIGLIORATO
 const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 const age = parseInt(prompt("Quanti anni hai?"));
 
-const ticketPrice = (km * 0.21).toFixed(2);
+let ticketPrice = (km * 0.21);
 
 console.log("km", km);
 console.log("age", age);
 
-if (!isNaN(km) && !isNaN(age))
+if (!isNaN(km) && !isNaN(age)) {
+    let ticketPriceCurrent = 0;
     if (age < 18) {
-        document.getElementById("price").innerHTML = ticketPrice * 4 / 5 + " Euro";
+        ticketPriceCurrent = ticketPrice * 4 / 5;
     } else if (age > 64) {
-        document.getElementById("price").innerHTML = ticketPrice * 3 / 5 + " Euro";
-    } else {
-        document.getElementById("price").innerHTML = ticketPrice + " Euro";
+        ticketPriceCurrent = ticketPrice * 3 / 5;
     }
+    document.getElementById("price").innerHTML = ticketPriceCurrent.toFixed(2) + " Euro";
+}
 else {
     document.getElementById("price").innerHTML = "Non hai inserito un numero!";
 }
