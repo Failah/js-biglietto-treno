a9114e5cd9e72b7ac66845a28d7423c7946fcca5
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-23 15:19:05 +0200

    Some adjustments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 const km = parseInt(prompt("Quanti chilometri vuoi percorrere?"));
 const age = parseInt(prompt("Quanti anni hai?"));
 
-const ticketPrice = km * 0.21;
+const ticketPrice = (km * 0.21).toFixed(2);
 
 console.log("km", km);
 console.log("age", age);
 
-if (age < 18) {
-    document.getElementById("price").innerHTML = ticketPrice * 4 / 5;
-} else if (age > 64) {
-    document.getElementById("price").innerHTML = ticketPrice * 3 / 5;
-} else {
-    document.getElementById("price").innerHTML = ticketPrice;
+if (!isNaN(km) && !isNaN(age))
+    if (age < 18) {
+        document.getElementById("price").innerHTML = ticketPrice * 4 / 5;
+    } else if (age > 64) {
+        document.getElementById("price").innerHTML = ticketPrice * 3 / 5;
+    } else {
+        document.getElementById("price").innerHTML = ticketPrice;
+    }
+else {
+    document.getElementById("price").innerHTML = "Non hai inserito un numero!";
 }
